1f93af197dd15df21fc1984a8b9ffbc239d2f889
ChristianBT96 <Christianbt95@gmail.com>
2023-11-14 15:04:33 +0100

    Added donut, not done.

--- main.js
+++ main.js
@@ -2,20 +2,26 @@ console.log("Hello World!")
 
 
 
+const chart4elemnet = document.querySelector('#chart-4').getContext('2d');
 
-const ctx = document.querySelector('#chart-1').getContext('2d');
-const chart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            data: [12, 19, 3, 5, 2, 3],
-        }]
-    }
-});
-
-
-
+const chart4Data = {
+    labels: ["red", "blue", "yellow"],
+    datasets: [{
+        label: "Warned",
+        data: [160092, 59812, 47965],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+    }]
+};
+const chart4Config = {
+    type: "doughnut",
+    data: chart4Data
+}
+const chart4 = new Chart(chart4elemnet, chart4Config);
 
 
 
