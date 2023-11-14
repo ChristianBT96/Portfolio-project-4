01221282a4430108260822d2d95a0c57077245b3
ChristianBT96 <Christianbt95@gmail.com>
2023-11-14 14:20:14 +0100

    Added lorem ipsum in intro, and a temp graph. With chart.js.

--- main.js
+++ main.js
@@ -3,7 +3,16 @@ console.log("Hello World!")
 
 
 
-
+const ctx = document.querySelector('#chart-1').getContext('2d');
+const chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            data: [12, 19, 3, 5, 2, 3],
+        }]
+    }
+});
 
 
 
